62ba1099ff10a447a08d243cf1167009e71be2b4
unknown <socgenb2java15@iiht.tech>
2021-12-03 18:31:45 +0530

    first commit

---
+++ src/main/java/com/example/mydemoapp/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.example.mydemoapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Environment environment;
+
+    @Autowired
+    public DemoController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getStatus()
+    {
+        return ResponseEntity.ok("Hello From helloController Listing on Port: "+environment.getProperty("local.server.port"));
+    }
+}
